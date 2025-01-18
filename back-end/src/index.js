56a66b0e07fdc8022d18c460d1cdee6c409fb9f2
saivishnureddybondugula@gmail.com <saivishnureddybondugula@gmail.com>
2025-01-18 22:26:48 +0530

    V

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,9 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -30,8 +28,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend","dist")));
+  // Serve static files from frontend/dist
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
+  // Serve the index.html for all other routes
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
