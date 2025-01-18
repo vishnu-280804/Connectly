023b2b35841e40a923e083bf8675a0fbd1ea75a5
saivishnureddybondugula@gmail.com <saivishnureddybondugula@gmail.com>
2025-01-18 22:53:04 +0530

    S

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Simplified version without rollupOptions
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, 'front-end', 'dist'),  // Output directory
-  },
-});
+})
